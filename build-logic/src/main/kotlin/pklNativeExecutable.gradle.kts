df13e6b8061d31729416173d15f3fdb1a9dc1489
Adam Thibert <adamthibert01@gmail.com>
2026-09-05 20:25:15 +0200

    Add ThreadStackSize argument for Alpine executable

--- build-logic/src/main/kotlin/pklNativeExecutable.gradle.kts
+++ build-logic/src/main/kotlin/pklNativeExecutable.gradle.kts
@@ -111,7 +111,7 @@ val linuxExecutableAarch64 =
 val alpineExecutableAmd64 =
   tasks.register<NativeImageBuild>("alpineExecutableAmd64") {
     configure(Target.AlpineLinuxAmd64)
-    extraNativeImageArgs.addAll("--static", "--libc=musl")
+    extraNativeImageArgs.addAll("--static", "--libc=musl", "-H:ThreadStackSize=8m")
   }
 
 val windowsExecutableAmd64 =
